15c025b3af7350b31dd9bc16d444480f12972691
RohanKD <77335983+RohanKD@users.noreply.github.com>
2023-01-05 20:38:34 -0500

    Delete primes.py

--- code/primes.py
+++
@@ -1,6 +0,0 @@
-if __name__=="__main__":
-    nums = []
-    nums.append(int(input()))
-    nums = tuple(nums) # sundara rajan michigan uni
-# venkata ramakrishnan g n ramachandran
-    print(len(nums))
